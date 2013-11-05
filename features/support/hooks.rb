ba00ef4992343d3460d47189294e011396c46d70
ASAD <asad.zaman@patch.com>
2013-11-05 16:59:01 -0500

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -9,7 +9,9 @@ Before do
     elsif ENV['BROWSER'] == 'ie'
       $browser = Watir::Browser.new :ie
     else
-      $browser = Watir::Browser.new :firefox
+      # $browser = Watir::Browser.new :firefox
+      $browser = Watir::Browser.new(:remote, :url => 'http://daemonp:2d3e8899-7ee6-4f82-a2a4-363b42a1caae@ondemand.saucelabs.com:80/wd/hub')
+      
     end
    
 end
